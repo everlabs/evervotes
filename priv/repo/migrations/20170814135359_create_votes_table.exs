c02c6cb7173f5e3e3c617683c524e1f4f0c362b6
DenysNorman <denys.norman.tr@gmail.com>
2017-08-14 16:59:00 +0300

    add votes table

---
+++ priv/repo/migrations/20170814135359_create_votes_table.exs
@@ -0,0 +1,9 @@
+defmodule EvervotesApi.Repo.Migrations.CreateVotesTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:votes) do
+      add :option_id, references(:options)
+    end
+  end
+end
